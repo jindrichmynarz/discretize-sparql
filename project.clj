0f73186ccd8090a8b8e999a635ddfe2a37f65081
Jindrich Mynarz <mynarzjindrich@gmail.com>
2017-04-05 08:45:25 +0200

    Bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject discretize-sparql "0.1.0"
+(defproject discretize-sparql "0.1.1"
   :description "Discretize numeric literals in RDF from a SPARQL endpoint"
   :url "http://github.com/jindrichmynarz/discretize-sparql"
   :license {:name "Eclipse Public License"
